e4c7ed0a7a2bdbdf28474bfa64a583c116987b1d
lucky777 <muphin1223@protonmail.com>
2021-10-05 17:53:28 +0200

    DON3 + ALG3

---
+++ ALG3/Linked Lists/Stack.java
@@ -0,0 +1,42 @@
+public class Stack {
+
+    private int[] tab;
+    private int topIndex;
+
+    public Stack(int maxSize) {
+        this.tab = new int[maxSize];
+        this.topIndex = -1;
+    }
+
+    public void push(int value) {
+        if (this.topIndex == (this.tab.length - 1)) {
+            System.out.println("error: stack is full!");
+        } else {
+            this.tab[++this.topIndex] = value;
+        }
+    }
+
+    public int pop() {
+        if (topIndex < 0) {
+            throw new ArrayIndexOutOfBoundsException("error: stack is empty!");
+        }
+        return this.tab[this.topIndex--];
+    }
+
+    public boolean isEmpty() {
+        return this.topIndex < 0;
+    }
+
+    public int top() {
+        if (this.topIndex < 0) {
+            System.out.println("error: stack is empty!");
+            return -1;
+        }
+
+        return this.tab[this.topIndex];
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("blblbl :-)");
+    }
+}
